c31e92816e782f64145c28d0debddbab7b84a25c
sofelaisrael <129384534+sofelaisrael@users.noreply.github.com>
2025-01-08 01:02:12 +0100

    frontend test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,51 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import FilteredProducts from './pages/FilteredProducts';
+import SingleProduct from './pages/SingleProduct';
+import Cart from './pages/Cart';
+import WishList from './pages/WishList';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/filteredProducts/",
+        element: <FilteredProducts />
+      },
+      {
+        path: "/filteredProducts/:id",
+        element: <SingleProduct />
+      },
+      {
+        path: "/cart",
+        element: <Cart />
+      },
+      {
+        path: "/wishlist",
+        element: <WishList />
+      },
+
+    ]
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
+);
+
